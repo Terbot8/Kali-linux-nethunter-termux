d220bdcb95e125c5302f87cd5113d90a1629f9a1
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-07-28 22:10:22 +0600

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,6 +1,7 @@
 apt remove sudo
 rm -rf /etc/resolv.conf
-echo '' nameserver 8.8.8.8 '' >> /etc/resolv.conf
+echo 'nameserver 8.8.8.8 ' >> /etc/resolv.conf
+echo 'nameserver 8.8.4.4 ' >> /etc/resolv.conf
 apt update
 apt install sudo udisks2 
 sudo rm /var/lib/dpkg/info/udisks2.postinst
