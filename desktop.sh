136e4b3895e850cf98c386434dd88e63047a0162
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-07-28 22:14:24 +0600

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -3,11 +3,12 @@ rm -rf /etc/resolv.conf
 echo 'nameserver 8.8.8.8 ' >> /etc/resolv.conf
 echo 'nameserver 8.8.4.4 ' >> /etc/resolv.conf
 apt update
-apt install sudo udisks2 
-sudo rm /var/lib/dpkg/info/udisks2.postinst
+apt install udisks2 
+rm /var/lib/dpkg/info/udisks2.postinst
 echo '''' >> /var/lib/dpkg/info/udisks2.postinst
-sudo dpkg --configure -a
+dpkg --configure -a
 apt-mark hold udisks2 
+apt install sudo
 sudo apt install xfce4 xfce4-whiskermenu-plugin -y
 sudo apt install kali-themes dbus-x11 qterminal firefox-esr tigervnc-standalone-server -y
 clear
