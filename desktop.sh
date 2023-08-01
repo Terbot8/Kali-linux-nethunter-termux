825942a3323fbaceee1d99966735e5867384c845
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-08-01 20:56:11 +0600

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,17 +1,18 @@
-apt remove sudo
+
 rm -rf /etc/resolv.conf
 echo 'nameserver 8.8.8.8 ' >> /etc/resolv.conf
 echo 'nameserver 8.8.4.4 ' >> /etc/resolv.conf
 apt update
-apt install sudo
+
 sudo apt install udisks2 
 rm /var/lib/dpkg/info/udisks2.postinst
 echo '''' >> /var/lib/dpkg/info/udisks2.postinst
 dpkg --configure -a
 apt-mark hold udisks2 
+sudo apt remove sudo
 apt install sudo
 sudo apt install xfce4 xfce4-whiskermenu-plugin -y
 sudo apt install kali-themes dbus-x11 qterminal firefox-esr tigervnc-standalone-server -y
 clear
 echo ' now vncserver and enter '
-sudo su kali
+exit
