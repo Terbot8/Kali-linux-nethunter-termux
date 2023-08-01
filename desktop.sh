8a27500e85cb2f6d3c25dc3aa191bc3ef41d5477
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-08-01 21:35:28 +0600

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,4 +1,7 @@
-
+echo "vncserver -kill :* ; rm -rf /tmp/.X1-lock ; rm -rf /tmp/.X11-unix/X1">> /bin/kex-stop
+chmod +x /bin/kex-stop
+echo "vncserver " >> /bin/kex
+chmod +x /bin/kex
 rm -rf /etc/resolv.conf
 echo 'nameserver 8.8.8.8 ' >> /etc/resolv.conf
 echo 'nameserver 8.8.4.4 ' >> /etc/resolv.conf
@@ -14,5 +17,4 @@ apt install sudo
 sudo apt install xfce4 xfce4-whiskermenu-plugin -y
 sudo apt install kali-themes dbus-x11 qterminal firefox-esr tigervnc-standalone-server -y
 clear
-echo ' now vncserver and enter '
 exit
