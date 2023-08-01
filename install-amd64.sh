98f559739cd02614951a8b6c16370b67bd28fa5e
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-08-01 21:04:55 +0600

    Update install-amd64.sh

--- install-amd64.sh
+++ install-amd64.sh
@@ -37,6 +37,24 @@ nh-r
 
 
 clear
+printf "${blue}##################################################\n"
+    printf "${blue}##                                              ##\n"
+    printf "${blue}##  88      a8P         db        88        88  ##\n"
+    printf "${blue}##  88    .88'         d88b       88        88  ##\n"
+    printf "${blue}##  88   88'          d8''8b      88        88  ##\n"
+    printf "${blue}##  88 d88           d8'  '8b     88        88  ##\n"
+    printf "${blue}##  8888'88.        d8YaaaaY8b    88        88  ##\n"
+    printf "${blue}##  88P   Y8b      d8''''''''8b   88        88  ##\n"
+    printf "${blue}##  88     '88.   d8'        '8b  88        88  ##\n"
+    printf "${blue}##  88       Y8b d8'          '8b 888888888 88  ##\n"
+    printf "${blue}##                                              ##\n"
+    printf "${blue}####  ############# NetHunter ####################${reset}\n\n"
+}
+
+
+##################################
+##              Main            ##
+
 printf "${blue} [+] nh = nethunter start${reset}\n "
 printf "${blue} [+] nh-r =nethunter start as root${reset}\n "
 
